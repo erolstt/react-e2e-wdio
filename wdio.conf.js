90157e0afa30cff18934ce96f6c13e0ca0cc15ce
erolstt <selitektay@gmail.com>
2018-01-04 10:04:29 +0000

    increase timeout

--- wdio.conf.js
+++ wdio.conf.js
@@ -140,7 +140,7 @@ exports.config = {
     // See the full list at http://mochajs.org/
     mochaOpts: {
         ui: 'bdd',
-        timeout: 20000
+        timeout: 9000000
     },
     //
     // =====
